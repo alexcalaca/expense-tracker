195986f6dcce1528773ec80ba98daa2b7a6138bc
Alexandre Calaça <alexandre.calaca@cilia.com.br>
2023-04-25 09:12:44 -0300

    feat: create api class

--- app/api.rb
+++ app/api.rb
@@ -1,12 +1,26 @@
+require 'sinatra/base'
+require 'json'
+require_relative 'ledger'
+
 module ExpenseTracker
-  require 'sinatra/base'
-  require 'json'
-
   class API < Sinatra::Base
-    post '/expenses' do
-      JSON.generate('expense_id' => 42)
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
     end
+    
+    post '/expenses' do
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
 
+      if result.success?
+        JSON.generate('expense_id' => result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end      
+    end
+                
     get '/expenses/:date' do
       JSON.generate([])
     end
